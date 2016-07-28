f85562d0ef9aab2865183b8d8f03e6ba5249c425
Douglas Zuqueto <douglas.zuqueto@gmail.com>
2016-07-28 17:19:01 +0000

    Add mqtt events

--- mqtt/subscriber-test.js
+++ mqtt/subscriber-test.js
@@ -7,6 +7,9 @@ subscriber.on('connect', () => {
     console.log('Connected Broker');
     subscriber.subscribe('test');
 });
+subscriber.on('reconnect', () => console.log('Retry'))
+
+subscriber.on('close', () => console.log('Close Connection'))
 
 subscriber.on('message', (topic, message) => {
     console.log('Topic:', topic);
