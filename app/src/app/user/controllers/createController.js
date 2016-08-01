8d11133b883c5a1a1aff8d7175b531be12e82d13
Douglas Zuqueto <douglas.zuqueto@gmail.com>
2016-08-01 20:11:18 -0300

    APP - User: Create User; APP - User: Remove User;

--- app/src/app/user/controllers/createController.js
+++ app/src/app/user/controllers/createController.js
@@ -1,8 +1,8 @@
 angular.module('app.user').controller('createController', createController);
 
-createController.$inject = ['$http', '$window'];
+createController.$inject = ['$http', '$window', '$state'];
 
-function createController($http, $window) {
+function createController($http, $window, $state) {
     var vm = this;
 
     vm.page = 'Cadastrar novo Usuário';
@@ -13,7 +13,13 @@ function createController($http, $window) {
      */
     vm.create = create;
     function create(user) {
-        console.log(user);
+        $http.post('http://localhost:3000/api/v1/users', user)
+            .then(function (response) {
+                var user = response.data;
+                $window.Materialize.toast('Usuário ' + user.name + 'foi criado', 1000, null, function () {
+                    $state.go('user.index');
+                });
+            });
     }
 
 }
--- app/src/app/user/controllers/indexController.js
+++ app/src/app/user/controllers/indexController.js
@@ -7,14 +7,28 @@ function indexController($http, $window) {
 
     vm.page = 'Usuários';
 
-    $http.get('http://localhost:3000/api/v1/users')
-        .then(function (response) {
-            vm.users = response.data;
-        });
+    /**
+     * Load Users
+     */
+    loadUsers();
+    function loadUsers() {
+        $http.get('http://localhost:3000/api/v1/users')
+            .then(function (response) {
+                vm.users = response.data;
+            });
+    }
 
+    /**
+     * Remove user
+     * @type {remove}
+     */
     vm.remove = remove;
     function remove(user) {
-        $window.Materialize.toast('Usuário ' + user.name + ' removido', 3000);
+        $http.delete('http://localhost:3000/api/v1/users/' + user._id)
+            .then(function (response) {
+                $window.Materialize.toast('Usuário ' + user.name + ' removido', 2000);
+                loadUsers();
+            });
     }
 
 }
