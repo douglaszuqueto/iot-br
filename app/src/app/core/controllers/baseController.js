94020f2e5db4f54ea49e1cc0f70b168132c43374
Douglas Zuqueto <douglas.zuqueto@gmail.com>
2016-08-01 20:47:11 -0300

    APP - Core: User Logout;

--- app/src/app/core/controllers/baseController.js
+++ app/src/app/core/controllers/baseController.js
@@ -1,10 +1,20 @@
 angular.module('app').controller('BaseController', BaseController);
 
-BaseController.$inject = [];
+BaseController.$inject = ['$state'];
 
-function BaseController() {
+function BaseController($state) {
     var vm = this;
 
     vm.user_logged = localStorage.getItem('user.email') || 'Usuário';
+
+    /**
+     * User Logout
+     * @type {logout}
+     */
+    vm.logout = logout;
+    function logout() {
+        localStorage.removeItem('token');
+        $state.go('login');
+    }
 };
 
