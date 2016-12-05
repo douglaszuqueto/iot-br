dbf273c75f414a3171925e455aceaea2c122522d
Jean Carlo Nascimento <jnascimento@gmail.com>
2016-12-05 11:02:06 -0200

    Update broker.js

--- broker/broker.js
+++ broker/broker.js
@@ -6,17 +6,18 @@ const broker = new mosca.Server(config.broker.port);
 /**
  * Events
  */
-broker.on('clientConnected', (client) => console.log('New Client is connected:'));
-
-broker.on('clientDisconnected', (client) => console.log('Client is disconnected:'));
-
-broker.on('published', (packet, client) => {
+const clientConnected = (client) => console.log('New Client is connected:');
+const clientDisconnected = (client) => console.log('Client is disconnected:');
+const published = (packet, client) => {
     console.log('Id:', packet.messageId);
     console.log('Topic:', packet.topic);
     console.log('Message:', packet.payload.toString());
     console.log('QoS:', packet.qos);
-});
+}
+const ready = () => console.log('Broker is Running');
 
-broker.on('ready', () => {
-    console.log('Broker is Running');
-});
+broker
+    .on('clientConnected', clientConnected);
+    .on('clientDisconnected', clientDisconnected);
+    .on('published', published);
+    .on('ready', ready);
